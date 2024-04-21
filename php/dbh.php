6f3e45e4154f55faa9ba3b36528d634f2597f0e9
sami855-ux <samitale56@outlook.com>
2024-04-21 21:15:01 +0500

    Added some comment abou mysqli connecting methods

--- php/dbh.php
+++ php/dbh.php
@@ -11,3 +11,28 @@ try {
 } catch (PDOException $e) {
     die("connection failed: " . $e->getMessage());
 }   
+
+
+/* We can also use myqli method to connect to the databse called myfristdatabse
+    Example 
+    <?php
+        $servername = "localhost"; // Change this to your MySQL server hostname
+        $username = "username"; // Change this to your MySQL username
+        $password = "password"; // Change this to your MySQL password
+        $database = "database_name"; // Change this to the name of your MySQL database
+
+        Create a connection to the MySQL database
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        Check the connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        echo "Connected successfully";
+
+        Close the connection
+        mysqli_close($conn);
+       
+
+*/ 
