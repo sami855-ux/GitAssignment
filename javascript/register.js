752e39bf7b2394d1da9dfb1fea52afa11538e599
biniyamNib <166550642+biniyamNib@users.noreply.github.com>
2024-04-21 05:52:06 -0700

    Update register.js
    javascript change of this code

--- javascript/register.js
+++ javascript/register.js
@@ -6,3 +6,65 @@ const confirmPass = document.getElementById("confiPass")
 const eduValue = document.querySelector(".edvalue")
 const bDate = document.querySelector("#bDate")
 const registerBtn = document.querySelector(".btn")
+//Checker of email
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  )
+}
+const formValidater = (e) => {
+  let value = e.target.value
+  if (value === "") {
+    e.target.style.borderColor = "red"
+    e.target.classList.add("error")
+  } else {
+    e.target.style.borderColor = "green"
+    e.target.classList.remove("error")
+  }
+}
+const emialChecker = (e) => {
+  let value = e.target.value
+  if (isEmail(value)) {
+    e.target.style.borderColor = "green"
+    e.target.classList.remove("error")
+  } else {
+    e.target.style.borderColor = "red"
+    e.target.classList.add("error")
+  }
+}
+const passwordChecker = (e) => {
+  if (password()) {
+    e.target.style.borderColor = "green"
+    e.target.classList.remove("error")
+  } else {
+    e.target.style.borderColor = "red"
+    e.target.classList.add("error")
+  }
+}
+function password() {
+  let pass1Value = createPass.value.trim()
+  let pass2Value = confirmPass.value.trim()
+
+  if (pass1Value === pass2Value) {
+    return true
+  } else {
+    return false
+  }
+}
+userName.addEventListener("keyup", formValidater)
+bDate.addEventListener("keyup", formValidater)
+
+email.addEventListener("keyup", function (e) {
+  formValidater(e)
+  emialChecker(e)
+})
+// createPass.addEventListener("keyup", formValidater)
+confirmPass.addEventListener("keyup", function (e) {
+  formValidater(e)
+  passwordChecker(e)
+})
+registerBtn.addEventListener("click", function (e) {
+  e.preventDefault()
+  console.log(bDate.value)
+  console.log(eduValue.value)
+})
