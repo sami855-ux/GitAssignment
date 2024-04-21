d496de1ee4c62ebee34a233ded8ac10ca011e105
Abebaw_Addis <abadis1221@gmail.com>
2024-04-21 10:40:36 -0700

    modifying main file

--- main.php
+++ main.php
@@ -12,7 +12,6 @@ if(!isset($_SESSION["user_id"])) {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/reset.css" />
     <link rel="stylesheet" href="css/main.css" />
     <title>Home-Page</title>
   </head>
@@ -158,15 +157,7 @@ if(!isset($_SESSION["user_id"])) {
         ?>
         </div>
        
-        <!-- 
-          <div class="poster">
-            <img src="upload/360_F.jpg" alt="profile" />
-            <b>Full Name</b>
-          </div>
-          <div class="main-content">
-          
-          </div>
-        </div> -->
+       
       </div>
     </div>
 
