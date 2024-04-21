b9cab1b26ce4050b79b1b29f521201752f225046
sami855-ux <158570827+sami855-ux@users.noreply.github.com>
2024-04-21 21:40:47 +0500

    added the file conent of login.php file
    Basic html and php codes for login page and the main page of the website

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'php/configSeesion.php';
+require_once 'php/login.view.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/styel.css" />
+    <title>LoginPage</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container">
+        <div class="box form-box">
+          <header>Login</header>
+          <section class="error">
+           
+                  <?php
+            check_login_erros();
+            ?>
+          </section>
+          <form action="php/signin.php" method="post">
+            <div class="field input">
+              <label for="username">UserName</label>
+              <input type="text" name="username" id="username" required />
+            </div>
+
+            <div class="field input">
+              <label for="password">Password</label>
+              <input type="password" name="password" id="password" required />
+            </div>
+
+            <div class="field">
+              <button type="submit" class="btn2">login</button>
+            </div>
+            <div class="link">
+              <p>
+                Don't have an account <a href="register.php">Register Now</a>
+              </p>
+              <p>
+               <a href="Changepwd.php">Forgot Password ?</a>
+              </p>
+            </div>
+          </form>
+       
+        </div>
+      
+      
+      </div>
+    
+    
+    </div>
+   
+    <script src="javascript/login.js"></script>
+  </body>
+</html>
